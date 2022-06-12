b0a4b51ef6190c49ea9e171f9815338f56235141
Shen Jiamin <shen_jiamin@comp.nus.edu.sg>
2022-06-13 03:22:14 +0800

    update file download

--- scripts/build.py
+++ scripts/build.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 import frontmatter
 
-
+WEBSITE = "https://www.shenjm.dev/slides/slide-01/1"
 BASE = "/slides"
 DIST = "dist"
 
@@ -26,22 +26,26 @@ def exec(args):
 
 
 if __name__ == "__main__":
+    with open("static/CNAME", "r") as f:
+        CNAME = f.read().strip()
+
     for path in Path.cwd().glob("*.md"):
         if not path.is_file():
             sys.exit(f"{path}: File not found.")
 
         rel = path.absolute().relative_to(Path.cwd()).with_suffix("")
-        dist = DIST / rel
-        base = BASE / rel
-        file_path = base.with_suffix(".pdf")
+        html_dist = DIST / rel
         file_dist = DIST / rel.with_suffix(".pdf")
 
+        html_base = BASE / rel
+        file_path = f'https://{CNAME}{html_base.with_suffix(".pdf")}'
+
         print("rel", rel, sep="\t")
-        print("dist", dist, sep="\t")
-        print("base", base, sep="\t")
+        print("html_dist", html_dist, sep="\t")
+        print("html_base", html_base, sep="\t")
         print("file_path", file_path, sep="\t")
         print("file_dist", file_dist, sep="\t")
 
         set_download_link(path, str(file_path))
-        exec([str(x) for x in ["slidev", "build", path, "--out", dist, "--base", base]])
+        exec([str(x) for x in ["slidev", "build", path, "--out", html_dist, "--base", html_base]])
         exec([str(x) for x in ["slidev", "export", path, "--output", file_dist]])
